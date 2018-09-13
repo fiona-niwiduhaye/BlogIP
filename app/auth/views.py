298f0e5c22fa1fdab162490419ec6c4c8848ccbd
IreriVikki <wambsviki@gmail.com>
2018-09-13 15:22:12 +0300

    set up a login function that logs a user in and redirects them to the home page once they log in, this will later be chaged to redirect to the dashbord so that the user can work on updating his/her profile details

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,18 @@
-from .forms import LoginForm, RegistrationForm
-from . import auth
 from flask import redirect, render_template, url_for
+from .forms import LoginForm, RegistrationForm
+from flask_login import login_user
+from ..models import User
+from . import auth
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+        return redirect(url_for('index.html', name=user.username))
     return render_template('auth/login.html', form=form)
 
 
