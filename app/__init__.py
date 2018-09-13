94bbfc2c70772edf8d08c63557bbc7a6049731fd
IreriVikki <wambsviki@gmail.com>
2018-09-13 14:19:14 +0300

    added a user model to the project and a pasword hashing and verification sysyte

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+# creating instances of flask extensions by initializing the flask extenstions
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+# telling the login manager where it should operate from
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
     app.config['SECRET_KEY'] = 'c3be71c065117cd56d5113506fd6fbf3'
+
+    # initializing flask extentions to the app by calling the initapp method on then and passing the app as the arguments to the app
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # registering blueprint
     from .main import main as main_bp
---
+++ app/models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user
+from . import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+    firstname = db.Column(db.String(30))
+    lastname = db.Column(db.String(30))
+    email = db.Column(db.String(30))
+    pass_hash = db.Column(db.String(255))
+    bio = db.Column(db.String)
+    profile_photo = db.Column(db.String, default='default.jpg')
+
+    @property
+    def password(self):
+        raise AttributeError('You cant access the password')
+
+    @password.setter
+    def password(self, password):
+        self.pass_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_hash, password)
+
+    def __repr__(self):
+        return f'User {self.id}, {self.username}, {self.email}'
