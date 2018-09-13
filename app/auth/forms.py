b2af39c65c846a22e8f1c3360a637474f7dba8ed
IreriVikki <wambsviki@gmail.com>
2018-09-13 10:53:29 +0300

    created a login form in the authentication blueprint

---
+++ app/auth/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+
+def LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+def RegistrationForm(FlaskForm):
+    pass
