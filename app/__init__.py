5941ab050c80c480626e46db2012c82dae1884a2
IreriVikki <wambsviki@gmail.com>
2018-09-13 11:40:01 +0300

    created the main blueprint and registered it to the main app

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ def create_app(config_name):
     app.config['SECRET_KEY'] = 'c3be71c065117cd56d5113506fd6fbf3'
 
     # registering blueprint
+    from .main import main as main_bp
+    app.register_blueprint(main_bp, url_prefix='/home')
     from .auth import auth as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/authenticate')
 
