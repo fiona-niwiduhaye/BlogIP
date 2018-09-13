94065a933c1af40b92ef835436398ba3c514458c
IreriVikki <wambsviki@gmail.com>
2018-09-13 12:39:15 +0300

    created a register form and rendered it to the register html template

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,5 +6,10 @@ from flask import redirect, render_template, url_for
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('auth/register.html', form=form)
